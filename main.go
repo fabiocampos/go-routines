bf3f972f930b0fdf1b97859dd15ff06c6e0f5e2b
Fabio Campos <fabiocamposrj@gmail.com>
2018-11-25 17:06:58 -0200

    Using Channels + Select

--- main.go
+++ main.go
@@ -6,16 +6,41 @@ import (
 )
 
 func main() {
-	go PlayGuitar("E A D G B e")
-	PlayDrums("CC1 BD CC2 BD")
+	guitarRiffChannel := make(chan string)
+	drumBeatChannel := make(chan string)
+	go PlayGuitar(guitarRiffChannel)
+	go PlayDrums(drumBeatChannel)
+	for {
+		select {
+		case guitarrRiff, isGuitarChannelOpen := <-guitarRiffChannel:
+			if isGuitarChannelOpen == true {
+				fmt.Println(guitarrRiff)
+			}
+		case drumBeat, isDrumChannelOpen := <-drumBeatChannel:
+			if isDrumChannelOpen == true {
+				fmt.Println(drumBeat)
+			}
+		case <-time.After(2 * time.Second):
+			close(guitarRiffChannel)
+			close(drumBeatChannel)
+			fmt.Println("######Aplauses!!!!!")
+			return
+		}
+	}
 }
 
-func PlayGuitar(riff string) {
-	fmt.Println(riff)
-	time.Sleep(600 * time.Millisecond)
+func PlayGuitar(riff chan<- string) {
+	riffs := []string{"E G A", "E D C", "D D E"}
+	for _, riffToPerform := range riffs {
+		riff <- riffToPerform
+		time.Sleep(500 * time.Millisecond)
+	}
 }
 
-func PlayDrums(beat string) {
-	fmt.Println(beat)
-	time.Sleep(300 * time.Millisecond)
+func PlayDrums(beat chan<- string) {
+	beats := []string{"CC1 BD CC2 BD", "CC2 BD CC1 DB", "CC1 LMT LT HFT"}
+	for _, beatToPerform := range beats {
+		beat <- beatToPerform
+		time.Sleep(250 * time.Millisecond)
+	}
 }
