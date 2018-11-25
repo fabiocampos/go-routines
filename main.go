78f0aa9987cb7ed4f0871d45fe998ccdea499d9b
Fabio Campos <fabiocamposrj@gmail.com>
2018-11-25 21:04:40 -0200

    Implementing multiple fibonacci calc using channels

--- main.go
+++ main.go
@@ -5,15 +5,48 @@ import (
 	"time"
 )
 
+type Fibonacci struct {
+	FibonacciRequest  int
+	FibonacciResponse int
+}
+
 func main() {
+	//CallFibonacciSync()
+	CallFibonacciAsync()
+}
+
+func CallFibonacciAsync() {
 	start := time.Now()
-	numbers := []int{50, 60, 70, 80}
+	numbers := []int{30, 50, 40, 52}
+	channel := make(chan Fibonacci, len(numbers))
+
 	for _, fibonacciToDiscover := range numbers {
-		fibonacci := CalcFibonacci(fibonacciToDiscover)
-		fmt.Printf("O fibonacci de %d é %d \n", fibonacciToDiscover, fibonacci)
+		go AsyncFibonacciService(fibonacciToDiscover, channel)
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		fib := <-channel
+		fmt.Printf("O fibonacci de %d é %d \n", fib.FibonacciRequest, fib.FibonacciResponse)
+	}
+	close(channel)
+	elapsed := time.Since(start)
+	fmt.Println(elapsed)
+}
+
+func CallFibonacciSync() {
+	start := time.Now()
+	numbers := []int{30, 50, 40, 52}
+	for _, fibonacciToDiscover := range numbers {
+		fib := CalcFibonacci(fibonacciToDiscover)
+		fmt.Printf("O fibonacci de %d é %d \n", fibonacciToDiscover, fib)
 	}
 	elapsed := time.Since(start)
 	fmt.Println(elapsed)
+}
+
+func AsyncFibonacciService(fibonacciToDiscover int, response chan<- Fibonacci) {
+	fibonacci := CalcFibonacci(fibonacciToDiscover)
+	response <- Fibonacci{FibonacciRequest: fibonacciToDiscover, FibonacciResponse: fibonacci}
 }
 
 func CalcFibonacci(n int) int {
